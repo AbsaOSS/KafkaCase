fa6963275ab38e1230d20caf7fe78747c06395e1
Miroslav Chomut (CZ) <Miroslav.Chomut@absa.africa>
2024-10-10 12:02:03 +0200

    #17 resource handling helper classes (#18)

---
+++ examples/src/main/scala/za/co/absa/kafkacase/examples/writer/CustomResourceHandling.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.kafkacase.examples.writer
+
+import io.circe.Encoder
+import za.co.absa.kafkacase.models.utils.ResourceHandler.withResource
+import za.co.absa.kafkacase.writer.WriterImpl
+
+import java.util.Properties
+
+object CustomResourceHandling {
+  def apply[T: Encoder](writerProps: Properties, topicName: String, sampleMessageToWrite: T): Unit = {
+    withResource(new WriterImpl[T](writerProps, topicName))(writer => {
+      writer.Write("sampleMessageKey1", sampleMessageToWrite)
+      writer.Write("sampleMessageKey2", sampleMessageToWrite)
+    })
+  }
+}
